1023f08fac4fb3381ea4cdbad6337cdc9ec2fac7
llaine <loic-laine@orange.fr>
2025-03-19 15:04:09 +0100

    refactor: remove custom logging levels from Utils class

--- src/utils.ts
+++ src/utils.ts
@@ -12,16 +12,6 @@ export class Utils {
     this.logger = winston.createLogger({
       level: "info",
       format: winston.format.json(),
-      levels: {
-        emerg: 0,
-        alert: 1,
-        crit: 2,
-        error: 3,
-        warning: 4,
-        notice: 5,
-        info: 6,
-        debug: 7
-      },
       transports: [
         new winston.transports.Console({
           format: winston.format.combine(
