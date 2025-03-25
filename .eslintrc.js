04ff433b08c1636a4e4bffde417c1bf29d65b05b
llaine <loic-laine@orange.fr>
2025-03-25 10:40:25 +0100

    feat: update ESLint configuration and add deprecation plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'deprecation'],
   extends: ['plugin:@typescript-eslint/recommended'],
   rules: {
     "@typescript-eslint/indent": ["warn", 2],
@@ -8,6 +8,15 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/member-ordering": "error",
-    "@typescript-eslint/ban-ts-comment": "off"
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/explicit-function-return-type":"off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "deprecation/deprecation": "warn",
   },
-}
+  parserOptions: {
+    "project": "./tsconfig.json"
+  }
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# @all41/server.types
+Types server that hosts generic controllers & repositories
